3b6fcb4ed139317bebb0f444fafe9a3432eececf
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-04-30 19:41:16 +0000

    Update Things.js

--- Things.js
+++ Things.js
@@ -5,8 +5,8 @@ function setCookie(cname, cvalue, exdays) {
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-    function getCookie(cname) {
-    var name = cname + "=";
+    function getCookie(place) {
+    var place = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
     for(var i = 0; i <ca.length; i++) {
@@ -14,8 +14,8 @@ function setCookie(cname, cvalue, exdays) {
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+        if (c.indexOf(place) == 0) {
+            return c.substring(place.length, c.length);
         }
     }
     return "";
