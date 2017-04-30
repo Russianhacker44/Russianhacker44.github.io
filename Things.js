28a0b7ca3690401e19ee5856e667c9b89ca6cd10
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-04-30 21:46:14 +0000

    Update Things.js

--- Things.js
+++ Things.js
@@ -1,63 +1,20 @@
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+
+
+
+
+
+var d = new Date();
+var day = d.getDate();
+var month = d.getMonth()+1;
+
+if(day < 10){
+    day = "0"+d.getDate();
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+if(month < 10){
+    month = "0"+eval(d.getMonth()+1);
 }
 
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-           setCookie("username", user, 30);
-       }
-    }
-}
-//Fri Aug 30 2013 4:36 pm
-console.log(formatAMPM(new Date()));
-
-//using your function (passing in date)
-function formatAMPM(date) {
-    // gets the hours
-    var hours = date.getHours();
-    // gets the day
-    var days = date.getDay();
-    // gets the month
-    var minutes = date.getMinutes();
-    // gets AM/PM
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    // converts hours to 12 hour instead of 24 hour
-    hours = hours % 12;
-    // converts 0 (midnight) to 12
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    // converts minutes to have leading 0
-    minutes = minutes < 10 ? '0'+ minutes : minutes;
-  
-    // the time string
-    var time = hours + ':' + minutes + ' ' + ampm;
-  
-    // gets the match for the date string we want
-    var match = date.toString().match(/\w{3} \w{3} \d{1,2} \d{4}/);
-  
-    //the result
-    return match[0] + ' ' + time;
-}
+var date = day+"."+month+"."+d.getFullYear();
+document.getElementById('date').innerHTML = date;
