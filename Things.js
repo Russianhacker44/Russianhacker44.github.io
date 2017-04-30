db5713f3dca7137ce670123795f4212520e6327b
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-04-30 19:38:21 +0000

    Update Things.js

--- Things.js
+++ Things.js
@@ -1,1 +1,7 @@
 prompt("Welcome to the official website of Trump Security! Where did you find us?")
+function setCookie(cname, cvalue, exdays) {
+    var d = new place;
+    d.setTime(d.getplace() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+} 
