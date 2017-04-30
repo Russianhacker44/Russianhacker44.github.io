37225ef839d9d1488e94d0e8a3cf5d4d58e49700
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-04-30 21:50:29 +0000

    Update Things.js

--- Things.js
+++ Things.js
@@ -1,20 +1,35 @@
+//Fri Aug 30 2013 4:36 pm
+console.log(formatAMPM(new Date()));
 
-
-
-
-
-
-var d = new Date();
-var day = d.getDate();
-var month = d.getMonth()+1;
-
-if(day < 10){
-    day = "0"+d.getDate();
+//using your function (passing in date)
+function formatAMPM(date) {
+    // gets the hours
+    var hours = date.getHours();
+    // gets the day
+    var days = date.getDay();
+    // gets the month
+    var minutes = date.getMinutes();
+    // gets AM/PM
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    // converts hours to 12 hour instead of 24 hour
+    hours = hours % 12;
+    // converts 0 (midnight) to 12
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    // converts minutes to have leading 0
+    minutes = minutes < 10 ? '0'+ minutes : minutes;
+  
+    // the time string
+    var time = hours + ':' + minutes + ' ' + ampm;
+  
+    // gets the match for the date string we want
+    var match = date.toString().match(/\w{3} \w{3} \d{1,2} \d{4}/);
+  
+    //the result
+    return match[0] + ' ' + time;
 }
 
-if(month < 10){
-    month = "0"+eval(d.getMonth()+1);
-}
 
-var date = day+"."+month+"."+d.getFullYear();
-document.getElementById('date').innerHTML = date;
+
+
+
+
