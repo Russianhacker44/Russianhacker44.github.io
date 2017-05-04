e462e7c63216ce8ea77233e4068881a6549b3624
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-05-04 14:09:57 -0500

    Update Ben.js

--- https/russianhacker44.github.io/about/Ben.js
+++ https/russianhacker44.github.io/about/Ben.js
@@ -1,25 +1,34 @@
 // START DATE SCRIPT
 
-var showdate	= "yes"  	// SHOW THE DATE | yes | no |
+var current_date = new Date ( );
 
-   if (showdate === "yes") {
-document.write('<div id="date-location">');
-var d=new Date();
-var weekday=new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
-var monthname=new Array('January','February','March','April','May','June','July','August','September','October','November','December');
-  var hourname=new Array('1', '2', '3','4','5','6','7','8','9','10','11','12');  
-       var minutename=new Array('1', '2', '3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60');
-      
-document.write('<span class="date-font">' + weekday[d.getDay()] + ' ');
-document.write(monthname[d.getMonth()] + ' ');
-      
-document.write(d.getDate() + '. ');
-      document.write(hourname[d.getHours()] + ' ');
-document.write(minutename[d.getminutes()] + ' ');
-      
-     
-      
-document.write(d.getFullYear())
-document.write('<br></span>')
-document.write('</div>');
+var month_names = new Array ( );
+month_names[month_names.length] = "January";
+month_names[month_names.length] = "February";
+month_names[month_names.length] = "March";
+month_names[month_names.length] = "April";
+month_names[month_names.length] = "May";
+month_names[month_names.length] = "June";
+month_names[month_names.length] = "July";
+month_names[month_names.length] = "August";
+month_names[month_names.length] = "September";
+month_names[month_names.length] = "October";
+month_names[month_names.length] = "November";
+month_names[month_names.length] = "December";
+
+var day_names = new Array ( );
+day_names[day_names.length] = "Sunday";
+day_names[day_names.length] = "Monday";
+day_names[day_names.length] = "Tuesday";
+day_names[day_names.length] = "Wednesday";
+day_names[day_names.length] = "Thursday";
+day_names[day_names.length] = "Friday";
+day_names[day_names.length] = "Saturday";
+
+document.write ( day_names[current_date.getDay()] );
+document.write ( ", " );
+document.write ( month_names[current_date.getMonth()] );
+document.write ( " " + current_date.getDate() );
+document.write ( " " );
+document.write ( " " + current_date.getFullYear() );
 }
