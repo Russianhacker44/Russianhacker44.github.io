7eb94ed63b9d2bd263742976ceb5524b6780fc09
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-05-05 12:46:22 -0500

    Update Ben.js

--- https/russianhacker44.github.io/about/Ben.js
+++ https/russianhacker44.github.io/about/Ben.js
@@ -5,3 +5,12 @@ var d = new Date(),
     months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
     days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+document.getElementById("Date").style.color = "blue";
+document.getElementById("Date").style.font.size = "20px";
+document.getElementById("Date").style.border = "1px solid #006699";
+document.getElementById("Date").style.padding = "4px";
+document.getElementById("Date").style.z.index = "2";
+document.getElementById("Date").style.text.align = "center";
+document.getElementById("Date").style.color = "#006699";
+document.getElementById("Date").style.font = "20px arial, sans-serif";
+document.getElementById("Date").style.font.weight = "bold";
